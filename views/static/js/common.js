dcc6f559d107cbf7404b20d86f2feb5c1c90de3c
zxiaojun <zxiaojun@123.cn>
2017-10-10 23:13:52 +0800

    登录页面的功能完成,并且修改了侧边栏的头像模板

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,16 @@
 
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$(function () {
+	//在进行模板的时候需要进行一下用户页面的判断 因为在登陆页面并不需要进行模板的添加,只有不是登录页面才需要获取cookie的数据进行模板的渲染;
+  if(location.pathname!="/dashboard/login"){
+    // 这里的location.pathname是location的一个属性,存放的是当前页面的路径地址;
+    
+    //从cookie中获取user的信息
+    var userinfo=$.cookie("userinfo");
+    userinfo=JSON.parse(userinfo);
+    
+    
+    //需要使用模板引擎进行侧边栏的渲染
+    var html =template("profile-tpl",userinfo);
+    $("#user-info").html(html);
+  }
+})
